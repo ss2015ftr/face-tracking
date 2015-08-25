36322d6c3897d74f81c66dbd5f1aa69536f6785a
Konnect <123>
2015-08-25 12:13:18 +0300

    ftr test

--- samples/Detecting.cpp
+++ samples/Detecting.cpp
@@ -1,8 +1,7 @@
 #include "Detecting.hpp"
 
-
- using namespace std;
- using namespace cv;
+using namespace std;
+using namespace cv;
 
 
 void Detector::Detect(const Mat& fr, std::vector<Rect> &fcs)
@@ -12,5 +11,3 @@ void Detector::Detect(const Mat& fr, std::vector<Rect> &fcs)
       equalizeHist( frame_gray, frame_gray );
 	  face_cascade.detectMultiScale( frame_gray, fcs, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 }
-
-
